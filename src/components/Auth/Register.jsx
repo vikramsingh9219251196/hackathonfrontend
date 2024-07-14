1f1e0ef6cc975a93bb424d75f703f5e7a0df8114
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-07-14 17:47:54 +0530

    Update Register.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -66,7 +66,7 @@ const Register = () => {
     e.preventDefault();
    
     try {
-        const response = await fetch("http://localhost:8080/api/v1/user/register", {
+        const response = await fetch("https://hackathonbackend-s7wr.onrender.com/api/v1/user/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
