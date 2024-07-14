2f202b2a5762e2da2606f9c82def0054c7a91c95
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-07-14 17:47:35 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:8080/api/v1/user/login", {
+      const response = await fetch("https://hackathonbackend-s7wr.onrender.com/api/v1/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
